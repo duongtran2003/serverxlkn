dd68edfe674873f1792a0fadf4fa60301be4d6a6
duongtran2003 <dt005250@gmail.com>
2024-04-06 21:20:25 +0700

    comment untested

---
+++ src/controllers/comment.ts
@@ -0,0 +1,126 @@
+import { Request, Response, request } from "express";
+import { Process } from "../models/process";
+import { Comment } from "../models/comment";
+
+class CommentController {
+  async create(req: Request, res: Response) {
+    const userId = res.locals.claims.userId;
+    const { requestId, comment } = req.body;
+    if (!requestId || !comment) {
+      return res.status(400).json({
+        "message": "thieu thong tin"
+      });
+    }
+    
+    const process = await Process.findOne({ requestId: requestId, peopleId: userId });
+    if (!process) {
+      return res.status(403).json({
+        "message": "ban khong du tham quyen",
+      });
+    }
+    
+    const newComment = {
+      requestId: requestId,
+      peopleId: userId,
+      comment: comment,
+    } 
+
+    Comment.create(newComment)
+    .then((comment) => {
+      return res.status(201).json(comment);
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        "message": "server error: " + err.message,
+      });      
+    });
+  }
+  
+  async index(req: Request, res: Response) {
+    const { peopleId, requestId } = req.query;
+
+    const queryField: any = {};
+
+    if (peopleId) {
+      queryField.peopleId = peopleId;
+    }
+    if (requestId) {
+      queryField.requestId = requestId;
+    }
+      
+    const comments = await Comment.find(queryField)
+    return res.status(200).json(comments);
+  }
+  
+  async update(req: Request, res: Response) {
+    const commentId = req.params.id;
+    const newComment = {
+      comment: req.body.comment || undefined,
+    } 
+    if (!commentId) {
+      return res.status(400).json({
+        "message": "thieu thong tin",
+      });
+    }
+    
+    const userId = res.locals.claims.userId;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(400).json({
+        "message": "sai id",
+      });
+    }
+    if (comment.peopleId != userId) {
+      return res.status(403).json({
+        "message": "ban khong du tham quyen",
+      });
+    }
+    
+    Comment.findByIdAndUpdate(commentId, newComment, { new: true })
+    .then((comment) => {
+      return res.status(200).json(comment);
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        "message": "server error: " + err.message,
+      });
+    });
+  }
+  
+  async delete(req: Request, res: Response) {
+    const userId = res.locals.claims.userId;
+    const commentId = req.params.commentId;
+    if (!commentId) {
+      return res.status(400).json({
+        "message": "thieu thong tin"
+      });
+    }
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(400).json({
+        "message": "sai id",
+      });
+    }
+    if (comment.peopleId != userId) {
+      return res.status(403).json({
+        "message": "ban khong du tham quyen",
+      });
+    }
+    
+    Comment.findByIdAndDelete(commentId)
+    .then(() => {
+      return res.status(200).json({
+        "message": "success",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        "message": "server error: " + err.message,
+      });
+    });
+  }
+}
+
+export {
+  CommentController
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { userRoute } from "./user";
 import { authRouter } from "./auth";
 import { actionRouter } from "./action";
 import { requestRouter } from "./request";
+import { categoryRouter } from "./category";
+import { commentRouter } from "./comment";
 
 const router = Router();
 
@@ -10,6 +12,8 @@ router.use('/users', userRoute);
 router.use('/auth', authRouter);
 router.use('/actions', actionRouter);
 router.use('/requests', requestRouter);
+router.use('/categories', categoryRouter);
+router.use('/comments', commentRouter);
 
 export {
   router
