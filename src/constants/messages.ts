1bbb9c0048fc399368ed012d251ae9f3c5bfd0b4
Vu Duy Anh <duyanh19122k3@gmail.com>
2024-04-21 15:47:57 +0700

    auth message & action messages

---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+export const ACTION_MESSAGES = {
+  TAO_ACTION_THANH_CONG: "Tạo action thành công",
+  KHONG_TIM_THAY_ACTION: "Không tìm thấy action",
+  LAY_RA_TOAN_BO_ACTION_THANH_CONG: "Lấy ra toàn bộ action thành công",
+};
+export const AUTH_MESSAGES = {
+  THIEU_THONG_TIN_VE_MAT_KHAU_HOAC_TAI_KHOAN: "Thiếu thông tin về mật khẩu hoặc tài khoản",
+  KHONG_TIM_THAY_USER: "Không tìm thấy user",
+  SAI_THONG_TIN_TAI_KHOAN_HOAC_MAT_KHAU: "Sai thông tin tài khoản hoặc mật khẩu",
+  KHONG_CO_JWT_SECRET: "Không có JWT secret",
+  LOGOUT_THANH_CONG: "Logout thành công",
+}
