e9dd7c2ef47f4061294b79ea816b5984853c406f
duongtran2003 <dt005250@gmail.com>
2024-05-04 17:56:28 +0700

    config cors to allow multiple origins

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,32 @@ import express from "express";
 import cors from 'cors';
 import { connect as dbConnect } from './config/dbConnect';
 import { router } from "./routes/index.routes";
-import swaggerUi from 'swagger-ui-express'
+// import swaggerUi from 'swagger-ui-express'
 import fs from 'fs'
-import YAML from 'yaml'
+// import YAML from 'yaml'
 
 
 let cookieParser = require('cookie-parser');
-const swaggerDocument = YAML.parse(fs.readFileSync('./api-docs.yaml', 'utf8'))
+// const swaggerDocument = YAML.parse(fs.readFileSync('./api-docs.yaml', 'utf8'))
 require('dotenv').config();
 
 let app = express();
 dbConnect();
 app.use(cookieParser());
+let origins: string[] = [];
+if (process.env.FRONTEND) {
+  origins = process.env.FRONTEND.split(' ');
+}
+else {
+  origins = ["http://localhost:3001"];
+}
 app.use(cors({
   credentials: true,
-  origin: process.env.FRONTEND || "http://localhost:3001",
+  origin: origins,
   methods: "GET, POST, OPTIONS, PUT, DELETE, PATCH",
 }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+console.log(origins);
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
